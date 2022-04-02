17b7b1f6ed9de8d35f08a09efde7a5c4d1076ee7
Alejandro Alvarez <92798860+Alejoab@users.noreply.github.com>
2022-04-01 19:58:49 -0500

    1

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -3,21 +3,19 @@ package classroom;
 public class Asignatura {
 
     String nombre;
-    String n = nombre;
     int codigoInterno;
     int codigoExterno;
     Tipo tipo;
 
     Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
     }
 
     Asignatura(int codigoInterno,Tipo tipo) {
         this("Sin nombre", codigoInterno, 0,tipo);
     }
 
-    Asignatura(int codigoExterno,Tipo tipo) {
+    Asignatura(Tipo tipo, int codigoExterno) {
         this("Sin nombre", 0, codigoExterno,tipo);
     }
 
@@ -39,15 +37,15 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    void cambiarDatos(int codigoInterno) {
+    void cambiarCodigoInterno(int codigoInterno) {
         this.codigoInterno = codigoInterno;
     }
 
-    void cambiarDatos(int codigoExterno) {
+    void cambiarCodigoExterno(int codigoExterno) {
         this.codigoExterno = codigoExterno;
     }
 
-    void cambiarDatos(String nombre) {
+    void cambiarNombre(String nombre) {
         this.nombre = nombre;
     }
 
--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,12 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
     Persona(long cedula, String nombre) {
@@ -31,8 +30,14 @@ public class Persona {
 
     Persona(String nombre) {
         this.nombre = "";
+        cedula = 1;
         totalPersonas++;
     }
+    
+    Persona(){
+    	nombre = "Sin nombre";
+    }
+    
     
     long getCedula() {
         return cedula;
